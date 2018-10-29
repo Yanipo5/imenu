01239889f8432873c4524ad294514835e100a5f3
yaniv <you@example.com>
2018-10-30 00:15:14 +0200

    added rules check

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,6 +1,6 @@
 export default {
   requried(s: string) {
-    return s ? true : "requried";
+    return s && s.replace(/\s/g, "").length ? true : "requried";
   },
   email(s: string) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,31 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import rules from "@/utils/rules.ts";
+const { requried, email } = rules;
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe("Check requried rule", () => {
+  test("expect rules.requried positive return", () => {
+    expect(requried("1")).toBe(true);
+    expect(requried("a")).toBe(true);
+    expect(requried("$")).toBe(true);
+    expect(requried(" a ")).toBe(true);
+  });
+
+  test("expect rules.requried negative return", () => {
+    expect(requried("")).toBe("requried");
+    expect(requried(" ")).toBe("requried");
+    expect(requried("     ")).toBe("requried");
+  });
+});
+
+describe("Check email rule", () => {
+  test("expect rules.email positive return", () => {
+    expect(email("a@a.com")).toBe(true);
+    expect(email("acsa@aqwe.com")).toBe(true);
+  });
+
+  test("expect rules.email negative return", () => {
+    expect(email("a@a,com")).toBe("not a valid email");
+    expect(email("a@com")).toBe("not a valid email");
+    expect(email("@com")).toBe("not a valid email");
+    expect(email("a@")).toBe("not a valid email");
+  });
+});
